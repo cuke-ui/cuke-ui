93ea61eae3d375488d75e5e5bc48f9df2c9796f1
cuke-ui <1359518268@qq.com>
2018-10-29 17:57:49 +0800

    feat:add

--- components/affix/__tests__/index.test.js
+++ components/affix/__tests__/index.test.js
@@ -1,4 +1,3 @@
-import assert from "power-assert";
 import React from "react";
 import { render } from "enzyme";
 import toJson from "enzyme-to-json";
@@ -17,13 +16,5 @@ describe("<Affix/>", () => {
       </div>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
-  });
-  it("should find cuke-affix classnames", () => {
-    const wrapper = render(
-      <Affix>
-        <div>affix</div>
-      </Affix>
-    );
-    assert(wrapper.find(".cuke-affix").length >= 1);
   });
 });
