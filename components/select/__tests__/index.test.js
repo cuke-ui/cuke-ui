26dc5a3fd7fbde4327516fcd8f4bcc1c803e544d
cuke-ui <1359518268@qq.com>
2018-11-19 18:31:17 +0800

    chore: 增加 Select 测试

--- components/select/__tests__/index.test.js
+++ components/select/__tests__/index.test.js
@@ -40,6 +40,21 @@ describe("<Select/>", () => {
     expect(onChange).not.toHaveBeenCalled();
   });
 
+  it("should can trigger click when disabled", () => {
+    const onChange = jest.fn();
+    const wrapper = shallow(
+      <Select onChange={onChange}>
+        <Select.Option value="黄瓜">黄瓜</Select.Option>
+        <Select.Option value="茄子">茄子</Select.Option>
+        <Select.Option value="番茄">番茄</Select.Option>
+      </Select>
+    );
+    wrapper.find(".cuke-select-input").simulate("click");
+    setTimeout(() => {
+      expect(onChange).toHaveBeenCalled();
+    });
+  });
+
   it("should render init default value", () => {
     const wrapper = shallow(
       <Select defaultValue="黄瓜">
@@ -62,5 +77,75 @@ describe("<Select/>", () => {
     );
     wrapper.find(".cuke-select").simulate("click");
     assert(wrapper.state().selectedValue === "黄瓜");
+  });
+
+  it("should trigger onChange when clicked", () => {
+    const onChange = jest.fn();
+    const wrapper = shallow(
+      <Select.Option value="茄子" onChange={onChange}>
+        茄子
+      </Select.Option>
+    );
+    wrapper.find(".cuke-select-option").simulate("click");
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it("should window cannot trigger click event when un mount", () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(
+      <Select value="黄瓜">
+        <Select.Option value="黄瓜">黄瓜</Select.Option>
+        <Select.Option value="茄子">茄子</Select.Option>
+        <Select.Option value="番茄">番茄</Select.Option>
+      </Select>
+    );
+    wrapper.unmount();
+    window.onclick = () => onClick;
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it("should panel cannot hidden click event when window click", () => {
+    const wrapper = shallow(
+      <Select value="黄瓜">
+        <Select.Option value="黄瓜">黄瓜</Select.Option>
+        <Select.Option value="茄子">茄子</Select.Option>
+        <Select.Option value="番茄">番茄</Select.Option>
+      </Select>
+    );
+
+    window.onclick();
+    assert(wrapper.state().visible === null);
+    wrapper.find(".cuke-select-input").simulate("click");
+    assert(wrapper.state().visible === true);
+    window.onclick();
+    assert(wrapper.state().visible === true);
+  });
+
+  it("should trigger panel visible change", () => {
+    const onPanelVisibleChange = jest.fn();
+    const wrapper = shallow(
+      <Select value="黄瓜">
+        <Select.Option value="黄瓜">黄瓜</Select.Option>
+        <Select.Option value="茄子">茄子</Select.Option>
+        <Select.Option value="番茄">番茄</Select.Option>
+      </Select>
+    );
+    wrapper.find(".cuke-select-input").simulate("click");
+    setTimeout(() => {
+      expect(onPanelVisibleChange).toHaveBeenCalled();
+    });
+  });
+
+  it("should cannot trigger panel visible change when disabled groups", () => {
+    const onPanelVisibleChange = jest.fn();
+    const wrapper = shallow(
+      <Select value="黄瓜">
+        <Select.Option value="黄瓜">黄瓜</Select.Option>
+        <Select.Option value="茄子">茄子</Select.Option>
+        <Select.Option value="番茄">番茄</Select.Option>
+      </Select>
+    );
+    wrapper.find(".cuke-select-input").simulate("click");
+    expect(onPanelVisibleChange).not.toHaveBeenCalled();
   });
 });
