82a3f39ed2442748fcfa2dd06dfa0efe706f5a64
cuke-ui <1359518268@qq.com>
2019-01-27 16:48:05 +0800

    chore: 覆盖率忽略 style.js

--- jest.config.js
+++ jest.config.js
@@ -10,14 +10,16 @@ module.exports = {
     "/components/form",
     "/components/wordPad",
     "/components/dropdown",
-    "/components/styles",
-    "/components/**/style.js"
+    "/components/styles"
   ],
   transformIgnorePatterns: ["/node_modules/", ".history/*", "lib", "dist"],
   modulePathIgnorePatterns: ["/.history/", "lib", "dist"],
   moduleDirectories: ["node_modules", ".", "src", "src/shared"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
-  collectCoverageFrom: ["components/**/*.{js,jsx}"],
+  collectCoverageFrom: [
+    "components/**/*.{js,jsx}",
+    "!components/**/style.{js,jsx}"
+  ],
   transform: {
     "^.+\\.jsx?$": "babel-jest"
   },
